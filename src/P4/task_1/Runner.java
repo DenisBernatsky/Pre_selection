da9fb85d566a03024851799654b707651c8ac693
Dzianis_Biarnatski <d.s.bernatsky@gmail.com>
2018-04-05 11:58:35 +0300

    fix mistakes

--- src/P4/task_1/Runner.java
+++ src/P4/task_1/Runner.java
@@ -4,7 +4,6 @@ import P4.task_1.cars.BaseCar;
 import P4.task_1.data.CarsDataSQL;
 import P4.task_1.data.CarsDataXML;
 import P4.task_1.taxis.Taxis;
-import P4.task_1.utils.DBUtils;
 import P4.task_1.utils.ReadAndWriteInFileUtils;
 
 import java.io.IOException;
@@ -20,8 +19,7 @@ public class Runner {
         CarsDataXML data = new CarsDataXML(FILE_XML_NAME);
         CarsDataSQL dataSQL = new CarsDataSQL();
         Taxis taxis = new Taxis();
-        DBUtils dbUtils = new DBUtils();
-        dbUtils.createDB();
+        dataSQL.createDB();
         ReadAndWriteInFileUtils readAndWriteInFile = new ReadAndWriteInFileUtils();
 
         // XMl get data
--- src/P4/task_1/data/CarsDataSQL.java
+++ src/P4/task_1/data/CarsDataSQL.java
@@ -1,6 +1,7 @@
 package P4.task_1.data;
 
 import P4.task_1.cars.BaseCar;
+import org.apache.derby.jdbc.EmbeddedDriver;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ public class CarsDataSQL extends BaseData implements DataInterface{
     private static final String DB_LOGIN = "admin";
     private static final String DB_PASSWORD = "admin";
     private static final String SQL_ALL_VALUES = "SELECT * FROM CARS";
+    private static final String DB_CREATE_URL = String.format("jdbc:derby:%s;create=true;user=%s;password=%s", DB_NAME, DB_LOGIN, DB_PASSWORD);
 
     public ArrayList<BaseCar> getCarListFromEntity() throws SQLException {
         Connection connection = DriverManager.getConnection(DB_URL, DB_LOGIN, DB_PASSWORD);
@@ -39,6 +41,47 @@ public class CarsDataSQL extends BaseData implements DataInterface{
         connection.close();
         statement.close();
     return carList;
+    }
+
+    public void createDB(){
+        if (!checkBdConnect()){
+            try {
+                DriverManager.registerDriver(new EmbeddedDriver());
+                Connection connection = DriverManager.getConnection(DB_CREATE_URL);
+                Statement statement = connection.createStatement();
+                statement.executeUpdate("CREATE TABLE cars (ID int GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), " +
+                        "NAME varchar(255), TYPE varchar(255), CONSUMPTION varchar(255),  PRICE varchar(255))");
+                statement.addBatch("INSERT INTO cars (NAME, TYPE, CONSUMPTION, PRICE) VALUES ('Volvo', 'Passenger Car', '7', '14500')");
+                statement.addBatch("INSERT INTO cars (NAME, TYPE, CONSUMPTION, PRICE) VALUES ('Citroen', 'Minibus Car', '12', '25500')");
+                statement.addBatch("INSERT INTO cars (NAME, TYPE, CONSUMPTION, PRICE) VALUES ('LADA', 'Minibus Car', '14', '26500')");
+                statement.addBatch("INSERT INTO cars (NAME, TYPE, CONSUMPTION, PRICE) VALUES ('VAZ', 'Truck', '22', '95080')");
+                statement.addBatch("INSERT INTO cars (NAME, TYPE, CONSUMPTION, PRICE) VALUES ('Mazda', 'Passenger Car', '11', '8700')");
+                statement.addBatch("INSERT INTO cars (NAME, TYPE, CONSUMPTION, PRICE) VALUES ('Volvo', 'Passenger Car', '7', '12500')");
+                statement.addBatch("INSERT INTO cars (NAME, TYPE, CONSUMPTION, PRICE) VALUES ('Citroen', 'Minibus Car', '12', '16500')");
+                statement.addBatch("INSERT INTO cars (NAME, TYPE, CONSUMPTION, PRICE) VALUES ('LADA', 'Minibus Car', '14', '6500')");
+                statement.addBatch("INSERT INTO cars (NAME, TYPE, CONSUMPTION, PRICE) VALUES ('MAN', 'Truck', '24', '45080')");
+                statement.addBatch("INSERT INTO cars (NAME, TYPE, CONSUMPTION, PRICE) VALUES ('Mazda', 'Passenger Car', '11', '12700')");
+                statement.executeBatch();
+                connection.close();
+                statement.close();
+                System.out.println("Data base is created");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            System.out.println("Data base already exist");
+        }
+    }
+
+    private boolean checkBdConnect(){
+        try {
+            DriverManager.getConnection(DB_URL, DB_LOGIN, DB_PASSWORD);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
 }
